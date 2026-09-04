a4fbd2e980f29642870b1f0ca39deabb9b1126b1
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-09-04 14:30:34 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.16%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,21 +4,19 @@ public:
         int n = nums.size();
         int left = 0;
         int right = n-1;
-
         while(left <= right)
         {
-            int mid = (left + right)/2;
+            int mid = (left + right) / 2;
             if(target == nums[mid]){
                 return mid;
             }
             else if(target > nums[mid]){
-                left=mid+1;
+                left = mid + 1;
             }
             else{
-                right=mid-1;
+                right = mid - 1;
             }
        }
-     
       return left;
- }
+    }
 };

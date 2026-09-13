af558041c1ddda9f25178c9219e45fda31168408
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-09-14 01:19:03 +0530

    Time: 0 ms (100%), Space: 21.1 MB (26.54%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) { 
+        for (int i = 1; i < matrix.size(); i++){
+            for (int j = 1; j < matrix[i].size(); j++){
+                if (i == 0 || j == 0){
+                    continue;
+                }
+                if (matrix[i][j] != matrix[i-1][j-1]){
+                    return false;
+                }              
+            }
+        }
+        return true;
+    }
+};

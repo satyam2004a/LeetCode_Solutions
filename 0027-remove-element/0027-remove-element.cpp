0d8d33f3f7a4e26d3704cbfc2aacc828db66d9ac
satyam2004a <satyam.raj.cs28@iilm.edu>
2026-09-22 14:47:43 +0530

    Time: 0 ms (100%), Space: 11.6 MB (83.91%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,28 +3,15 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int i = 0;
         int j = 0;
-        while(i < nums.size()){
-            if(nums[i] != val){
-                i++;
-            }
-            else{
-                break;
-            }
-        }
-        j = i;
-
         while(j < nums.size()){
-            if (nums[j] == val){
-                j++;
-            }
-            else{
+            if(nums[j] != val){
                 swap(nums[i], nums[j]);
                 i++;
             }
+            j++;
         }
-        return i;
 
-        
+        return i;       
         
     }
 };
